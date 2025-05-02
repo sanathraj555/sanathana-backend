34d0c85a299b70a10b2c5eeb84a3db1c2b0f4994
sanatharaj555 <sanathmeesaraganda1@gmail.com>
2025-05-02 18:47:17 +0530

    added db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="sanathanamysql.mysql.database.azure.com",
         user="techlabs@sanathanamysql",
-        password="sanathanatechlabs@123",
+        password="labs@123",
         database="sanathana_chatbot_db",
         ssl_ca=os.path.abspath("DigiCertGlobalRootG2.crt.pem")
     )
