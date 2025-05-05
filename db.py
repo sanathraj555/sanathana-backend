862916031215aa93ea1d7af5e88075bcc490093c
sanatharaj555 <sanathmeesaraganda1@gmail.com>
2025-05-05 10:52:00 +0530

    Fixed SSL cert path for Azure MySQL connection

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import mysql.connector
 import os
 
+# Get the absolute path to the current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def get_db_connection():
     return mysql.connector.connect(
         host="sanathanamysql.mysql.database.azure.com",
         user="techlabs@sanathanamysql",
         password="labs@123",
         database="sanathana_chatbot_db",
-        ssl_ca=os.path.abspath("DigiCertGlobalRootG2.crt.pem")
+        ssl_ca=os.path.join(BASE_DIR, "DigiCertGlobalRootG2.crt.pem")  # Correct relative path
     )
