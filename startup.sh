5d5d7ce28771df88ba9c8a69e5c1f9ca908e9f13
sanatharaj555 <sanathmeesaraganda1@gmail.com>
2025-05-06 17:56:10 +0530

    startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-gunicorn app:app \
-  --bind=0.0.0.0:8000 \
-  --timeout 600 \
-  --capture-output \
-  --log-level debug
+gunicorn app:app -b 0.0.0.0:8000 --timeout 300 --log-level info --capture-output
