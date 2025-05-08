45aee6e47cede1c6fab474dd36dec0097edccd15
sanatharaj555 <sanathmeesaraganda1@gmail.com>
2025-05-08 13:48:49 +0530

    updated codes

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_db_connection():
     return mysql.connector.connect(
         host="sanathanamysql.mysql.database.azure.com",
-        user="techlabs@sanathanamysql",
+        user="techlabs",
         password="labs@123",  # Replace with actual password
         database="sanathana_chatbot_db",
         ssl_ca=os.path.join(BASE_DIR, "DigiCertGlobalRootG2.crt.pem")
